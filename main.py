72a06f18b0c8822c01278173fb299393a68c1252
myoshioka <y.mako2@gmail.com>
2020-09-08 20:05:43 +0900

    不要なコメントアウトを削除

--- main.py
+++ main.py
@@ -34,10 +34,4 @@ async def sleepapp():
 
 
 if __name__ == '__main__':
-    # asyncio.run(
-    #     uvicorn.run(app,
-    #                 host="0.0.0.0",
-    #                 port=3000,
-    #                 loop='asyncio',
-    #                 debug=False))
     uvicorn.run('main:app', host='0.0.0.0', port=3000, log_level='info')
