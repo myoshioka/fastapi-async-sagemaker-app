bce06410a705b78f64145aec4908f95a92efd5fe
myoshioka <y.mako2@gmail.com>
2021-01-06 21:19:52 +0900

    asyncを追加

--- sagemaker_endpoint.py
+++ sagemaker_endpoint.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
 from time import sleep
 import uvicorn
+from datetime import datetime
 
 app = FastAPI()
 
 
 @app.get('/ping')
-def ping():
+async def ping():
     return {"message": "ok"}
 
 
 @app.post('/invocations')
-def invocations():
+async def invocations():
+    print(f"{datetime.now():%H:%M:%S} sleep start.")
     sleep(5)
+    print(f"{datetime.now():%H:%M:%S} sleep end.")
     return {"message": "finish"}
 
 
